54883b0c33fb0e8cba6535c11720f8720f2dd250
Raj <2k22.psit.csds.32072@gmail.com>
2025-12-25 20:50:48 +0530

    Remove Groq AI chat API

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,34 @@
-"use client"
+"use client";
+
 import AboutMeSection from "@/components/AboutMeSection";
 import EmailSection from "@/components/EmailSection";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import ProjectSection from "@/components/ProjectSection";
-import AiChatbotSection from "@/components/AiChatbotSection";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col bg-[#121212] container sm:mx-auto lg:px-12 py-4 w-full  ">
-      <Navbar/>
-    <div className="lg:container mx-auto px-12 py-4  ">
-   <HeroSection/>
-  <section id="about" className="scroll-mt-24">
-  <AboutMeSection />
-</section>
-   <section id="projects" className="scroll-mt-24">
-  <ProjectSection />
-</section>
-   <section id="contact" className="scroll-mt-24">
-  <EmailSection />
-</section>
- <section id="chatbot" className="scroll-mt-24">
-  <AiChatbotSection />
-</section>
-   </div>  
-   <Footer/>
+    <div className="flex min-h-screen flex-col bg-[#121212] container sm:mx-auto lg:px-12 py-4 w-full">
+      <Navbar />
+
+      <div className="lg:container mx-auto px-12 py-4">
+        <HeroSection />
+
+        <section id="about" className="scroll-mt-24">
+          <AboutMeSection />
+        </section>
+
+        <section id="projects" className="scroll-mt-24">
+          <ProjectSection />
+        </section>
+
+        <section id="contact" className="scroll-mt-24">
+          <EmailSection />
+        </section>
+      </div>
+
+      <Footer />
     </div>
   );
 }
